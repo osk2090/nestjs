d16c56aaf2da99c9600df4a8ce3e0387b2bbb8c6
osk2090 <osk2090@naver.com>
2022-07-08 00:41:16 +0900

    module & capsule

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import {Body, Controller, Get, Param, Req} from '@nestjs/common';
+import { AppService } from './app.service';
+import {Request} from "express";
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {
+
+  }
+
+  @Get()
+  getHello() {
+    return this.appService.getHello();
+  }
+}
