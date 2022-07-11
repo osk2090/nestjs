0881a96d43769839b50dd3ac7ef9ba1928d331c8
buzz <buzz2090@naver.com>
2022-07-11 22:53:45 +0900

    connect MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './logger.middleware';
+import {MongooseModule} from "@nestjs/mongoose";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CatsModule, MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreatedIndex: true,
+      useFindAndModify: false,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
