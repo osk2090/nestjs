afa54c4009cc2a7242d73ed7755de60f28979773
buzz <buzz2090@naver.com>
2022-07-17 22:40:19 +0900

    like & reply service

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -3,11 +3,13 @@ import { CommentsController } from './controllers/comments.controller';
 import { CommentsService } from './services/comments.service';
 import {MongooseModule} from "@nestjs/mongoose";
 import {Comments, CommentsSchema} from "./comments.schema";
+import {CatsModule} from "../cats/cats.module";
 
 @Module({
   imports: [MongooseModule.forFeature([
     {name: Comments.name, schema: CommentsSchema}
-  ])],
+  ]),
+    CatsModule],
   controllers: [CommentsController],
   providers: [CommentsService]
 })
